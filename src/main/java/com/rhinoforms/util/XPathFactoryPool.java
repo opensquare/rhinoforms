edf51047fc3a8f3d5447a949465783f468fe9edf
Adam Knight <adam.knight@opensquare.co.uk>
2014-04-14 14:37:33 +0100

    Made XPathFactoryPool thread safe

--- src/main/java/com/rhinoforms/util/XPathFactoryPool.java
+++ src/main/java/com/rhinoforms/util/XPathFactoryPool.java
@@ -6,20 +6,23 @@ import javax.xml.xpath.XPathFactory;
 public class XPathFactoryPool {
 
 	private Stack<XPathFactory> pool = new Stack<XPathFactory>();
-	
+
 	public XPathFactoryPool() {
 	}
 
-	public XPathFactory getInstance(){
-		if(pool.isEmpty()){
-			return XPathFactory.newInstance();
-		}else{
-			return pool.pop();
+	public XPathFactory getInstance() {
+		if (!pool.isEmpty()) {
+			XPathFactory factory = pool.pop();
+			// Thread safe check
+			if (factory != null) {
+				return factory;
+			}
 		}
+		return XPathFactory.newInstance();
 	}
-	
-	public void returnInstance(XPathFactory xPathFactory){
+
+	public void returnInstance(XPathFactory xPathFactory) {
 		pool.push(xPathFactory);
 	}
-	
+
 }
